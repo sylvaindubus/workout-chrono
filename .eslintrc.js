1f4be2b5ea5805bc35ee384c05d7010a0668f763
Sylvain Dubus <svn.dbs@gmail.com>
2021-03-12 19:08:16 +0100

    Improve PWA compliance

--- .eslintrc.js
+++ .eslintrc.js
@@ -14,7 +14,7 @@ module.exports = {
   rules: {
     'no-console': process.env.NODE_ENV === 'production' ? 'warn' : 'off',
     'no-debugger': process.env.NODE_ENV === 'production' ? 'warn' : 'off',
-    '@typescript-eslint/no-unused-vars': [2, { args: 'after-used', argsIgnorePattern: '^_'}],
+    '@typescript-eslint/no-unused-vars': [2, { args: 'after-used', argsIgnorePattern: '^_' }],
     'no-useless-constructor': 'off',
     '@typescript-eslint/no-useless-constructor': ['error']
   }
--- src/main.ts
+++ src/main.ts
@@ -3,5 +3,6 @@ import { createApp } from 'vue'
 import App from './components/App.vue'
 import './assets/styles/reset.css'
 import './assets/styles/global.css'
+import './registerServiceWorker'
 
 createApp(App).mount('#app')
