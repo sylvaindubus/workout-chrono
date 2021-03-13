0a8ffd8fcb093b3c680b9ac7d1ef12923315ffdc
Sylvain Dubus <svn.dbs@gmail.com>
2021-03-13 22:01:36 +0100

    Fix PWA icon generation

--- vue.config.js
+++ vue.config.js
@@ -4,5 +4,16 @@ module.exports = {
       entry: 'src/main.ts',
       title: 'Workout Chrono'
     }
+  },
+  pwa: {
+    iconPaths: {
+      faviconSVG: 'favicon.svg',
+      favicon32: 'favicon-32x32.png',
+      favicon16: 'favicon-16x16.png',
+      appleTouchIcon: 'apple-touch-icon.png',
+      maskIcon: 'safari-pinned-tab.svg',
+      msTileImage: 'mstile-150x150.png'
+    },
+    msTileColor: '#4b6584'
   }
 }
